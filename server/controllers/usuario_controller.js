72f4b81cc7680e469fb29c3548af6aa4d2959412
FelipeSSM <felipesergiotrab@gmail.com>
2024-01-31 12:28:17 -0300

    refatoracao controller e route

--- server/controllers/usuario_controller.js
+++ server/controllers/usuario_controller.js
@@ -1,19 +1,39 @@
 const usuario_model = require("../models/usuario_model");
 class UsuarioController {
-  buscar() {
-    return usuario_model.listar();
+  listar(req, res) {
+    const usuarios = usuario_model.listar();
+    return usuarios
+      .then((usuario) => res.status(200).json(usuario))
+      .catch((error) => res.status(400).json(error.message));
   }
-  buscarPorId(id) {
-    return usuario_model.listarPorId(id);
+  buscarPorId(req, res) {
+    const { id } = req.params;
+    const usuario = usuario_model.buscarPorId(id);
+    return usuario
+      .then((usuariores) => res.status(200).json(usuariores))
+      .catch((error) => res.status(400).json(error.message));
   }
-  criar(novoUsuario) {
-    return usuario_model.criar(novoUsuario);
+  criar(req, res) {
+    const novoUsuario = req.body;
+    const usuario = usuario_model.criar(novoUsuario);
+    return usuario
+      .then((usuarioCriado) => res.status(201).json(usuarioCriado))
+      .catch((error) => res.status(400).json(error.message));
   }
-  atualizar(usuario_atualizado, id) {
-    return usuario_model.atualizar(usuario_atualizado, id);
+  atualizar(req, res) {
+    const { id } = req.params;
+    const usuario_atualizado = req.body;
+    const usuario = usuario_model.atualizar(usuario_atualizado, id);
+    return usuario
+      .then((usuarioAtualizado) => res.status(200).json(usuarioAtualizado))
+      .catch((error) => res.status(400).json(error.message));
   }
-  deletar(id) {
-    return usuario_model.deletar(id);
+  deletar(req, res) {
+    const { id } = req.params;
+    const usuario = usuario_model.deletar(id);
+    return usuario
+      .then((usuarioDeletado) => res.status(200).json(usuarioDeletado))
+      .catch((error) => res.status(400).json(error.message));
   }
 }
 
--- server/routers/usuario_route.js
+++ server/routers/usuario_route.js
@@ -2,49 +2,14 @@ const { Router } = require("express");
 const router = Router();
 const usuarioController = require("../controllers/usuario_controller");
 
-router.get("/", (req, res) => {
-  res.send("Olá Mundo!");
-});
+router.get("/usuario", usuarioController.listar);
 
-router.get("/usuario", (req, res) => {
-  const usuarios = usuarioController.buscar();
-  usuarios
-    .then((usuario) => res.status(200).json(usuario))
-    .catch((error) => res.status(400).json(error.message));
-});
+router.get("/usuario/:id", usuarioController.buscarPorId);
 
-router.get("/usuario/:id", (req, res) => {
-  const { id } = req.params;
-  const usuario = usuarioController.buscarPorId(id);
-  usuario
-    .then((usuariores) => res.status(200).json(usuariores))
-    .catch((error) => res.status(400).json(error.message));
-});
+router.post("/usuario", usuarioController.criar);
 
-router.post("/usuario", (req, res) => {
-  const novoUsuario = req.body;
+router.put("/usuario/:id", usuarioController.atualizar);
 
-  const usuario = usuarioController.criar(novoUsuario);
-  usuario
-    .then((usuarioCriado) => res.status(201).json(usuarioCriado))
-    .catch((error) => res.status(400).json(error.message));
-});
-
-router.put("/usuario/:id", (req, res) => {
-  const { id } = req.params;
-  const usuario_atualizado = req.body;
-  const usuario = usuarioController.atualizar(usuario_atualizado, id);
-  usuario
-    .then((usuarioAtualizado) => res.status(200).json(usuarioAtualizado))
-    .catch((error) => res.status(400).json(error.message));
-});
-
-router.delete("/usuario/:id", (req, res) => {
-  const { id } = req.params;
-  const usuario = usuarioController.deletar(id);
-  usuario
-    .then((usuarioDeletado) => res.status(200).json(usuarioDeletado))
-    .catch((error) => res.status(400).json(error.message));
-});
+router.delete("/usuario/:id", usuarioController.deletar);
 
 module.exports = router;
