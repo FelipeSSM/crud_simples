32c77a53a5a78aee58a1527f92ad4555e9bcff24
FelipeSSM <felipesergiotrab@gmail.com>
2024-01-31 23:31:38 -0300

    tabela funcionarios

--- client/src/components/Funcionarios.js
+++ client/src/components/Funcionarios.js
@@ -1,9 +1,37 @@
-function Funcionarios() {
-  return (
-    <div>
-      <p>Esta e a pagina dos Funcionarios</p>
-    </div>
-  );
+import React from "react";
+import { Table } from "react-bootstrap";
+class Funcionarios extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      alunos: [],
+    };
+  }
+
+  render() {
+    return (
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Cpf</th>
+            <th>Endereço</th>
+            <th>Opções</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Felipe</td>
+            <td>Felipe20@email.com</td>
+            <td>15478544203</td>
+            <td>Rua tal 420 tres fodacezoes</td>
+            <td>Atualizar, Excluir</td>
+          </tr>
+        </tbody>
+      </Table>
+    );
+  }
 }
 
 export default Funcionarios;
